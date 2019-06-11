8c892adb4f3087bbdbd8f400887127341d714fd3
Michiel van Leening <michiel@screencom.eu>
2019-06-11 15:27:54 +0200

    fix curl 302

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if (( $EUID != 0 )); then
     exit 1
 fi
 
-if [ -z "$1" ]; then
+if [[ -z "$1" ]]; then
     echo "./install.sh <your_authtoken>"
     exit 1
 fi
@@ -15,13 +15,16 @@ mkdir -p /opt/ngrok
 cd /opt/ngrok
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
 
-if [[ ! -f ngrok-stable-linux-arm.zip ]]; then
+if [[ -f ngrok-stable-linux-arm.zip ]]; then
     unzip ngrok-stable-linux-arm.zip
     rm -f ngrok-stable-linux-arm.zip
+else
+    echo "Unable to download ngrok..."
+    exit 1
 fi
 
-curl -so /lib/systemd/system/ngrok.service https://github.com/screencom/systemd-ngrok/raw/master/ngrok.service
-curl -so /opt/ngrok/ngrok.yml https://github.com/screencom/systemd-ngrok/raw/master/ngrok.yml
+curl -Lso /lib/systemd/system/ngrok.service https://github.com/screencom/systemd-ngrok/raw/master/ngrok.service
+curl -Lso /opt/ngrok/ngrok.yml https://github.com/screencom/systemd-ngrok/raw/master/ngrok.yml
 sed -i "s/<add_your_token_here>/$1/g" /opt/ngrok/ngrok.yml
 
 systemctl enable ngrok.service
